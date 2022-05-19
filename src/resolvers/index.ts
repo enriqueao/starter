c9fdf4e204b75956afef9700a6a476520b2f8695
Enrique Aguilar <enrique.aguilar.ext@kavak.com>
2022-05-18 21:19:58 -0500

    First commit

---
+++ src/resolvers/index.ts
@@ -0,0 +1,6 @@
+import { UserMutation } from './mutation/user/user'
+
+
+export const resolvers = [
+    UserMutation
+]
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import * as tq from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+import { GraphQLScalarType } from 'graphql';
+import { DateTimeResolver } from 'graphql-scalars';
+
+import { context } from './config/context';
+
+import { resolvers } from './resolvers';
+
+const app = async () => {
+    const schema = await tq.buildSchema({
+        resolvers,
+        scalarsMap: [{ type: GraphQLScalarType, scalar: DateTimeResolver }]
+    })
+
+    new ApolloServer({schema, context}).listen({ port: 4000 }, () => {
+        console.log('server ready 🚀');
+    })
+}
+
+app();
